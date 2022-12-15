61c574eb7a9f081cdfe83128ddff5b72c3c254e1
Pablo Monardo <pablomonardo@gmail.com>
2022-12-15 19:32:44 -0300

    Create math_operations.py

---
+++ Python/math_operations.py
@@ -0,0 +1,40 @@
+
+class Statistics:
+    def __init__(self, sample:list):
+        self.sample = sample
+    def arithmetical_mean(self):
+        return sum(self.sample)/len(self.sample)
+    def mode(self):
+        values = {}
+        for n in self.sample:
+            if n in values:
+                values[n]+=1
+            else:
+                values[n]=1
+        print(values)
+        mode= 0
+        pos = 0
+        for n in values:
+            if values[n]>mode:
+                pos=n
+                mode=values[n]
+
+        f_mode= []
+        for n in values:
+            if values[n]==mode:
+                f_mode.append(n)
+        
+        return pos if len(f_mode)==1 else f_mode
+    def median(self):
+        quantity = len(self.sample)
+        self.sample.sort()
+        if quantity%2 != 0:
+            return self.sample[(quantity//2)-1]
+        else:
+            return self.sample[((quantity//2 -1)+(quantity//2))/2]
+
+grupo = Statistics([10,5,12,3,14]) #5 valores, 12 es la media
+#grupo2 = Statistics([4,6,5,7,3,9]) #5,5 de media
+
+print(grupo.median())
+#print(grupo2.median())
