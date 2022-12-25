910d149ea71060cca4b5e63e305c6a91d0ae73b6
Pablo Monardo <pablomonardo@gmail.com>
2022-12-25 10:51:00 -0300

    Update fractions.py

--- Python/fractions.py
+++ Python/fractions.py
@@ -1,9 +1,9 @@
 class Fraction:
-    def __init__(self, dividend, divisor) -> None:
+    def __init__(self, dividend:int, divisor:int) -> None:
         self.dividend = dividend
         self.divisor = divisor
         self.nominator, self.denominator = self.get_fract()
-        
+
     def get_fract(self)->tuple:
         nominator = self.dividend
         denominator = self.divisor
@@ -24,7 +24,34 @@ class Fraction:
             else:
                 break
         
-        return nominator, denominator
+        return int(nominator), int(denominator)
+    
+    def __add__(self, fract):
+        """
+        This function adds two fractions together and returns a new one. 
+        """
+        new_nominator = (self.nominator*fract.denominator)+(fract.nominator*self.denominator)
+        new_denominator = self.denominator*fract.denominator
+
+        return Fraction(new_nominator, new_denominator)
+    
+    def __sub__(self, fract):
+        """
+        This function subtracts two fractions together and returns a new one. 
+        """
+        new_nominator = (self.nominator*fract.denominator)-(fract.nominator*self.denominator)
+        new_denominator = self.denominator*fract.denominator
+
+        return Fraction(new_nominator, new_denominator)
 
     def __str__(self) -> str:
         return f"{self.nominator}/{self.denominator}"
+
+a = Fraction(950,350)
+print(a)
+
+b = Fraction(480,645)
+print(b)
+
+c = a-b
+print(c)
